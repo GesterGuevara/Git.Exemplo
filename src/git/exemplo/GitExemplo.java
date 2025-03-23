eae251e3d893d9060b60a98837775f65a3c45644
GesterGuevara <gesguevara25@gmail.com>
2025-03-23 08:06:15 -0300

    Segunda versao do projeto

--- src/git/exemplo/GitExemplo.java
+++ src/git/exemplo/GitExemplo.java
@@ -6,7 +6,8 @@ public class GitExemplo {
 
     
     public static void main(String[] args) {
-        
+        System.out.println("Segunda versao ");
+        System.out.println("Textando envio");
     }
     
 }
